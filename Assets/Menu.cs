b3afb7dcf1b8ee849334ba506bdcb4a7608aca78
Daniil Komov <daniil_komov@icloud.com>
2026-04-28 01:37:08 +0300

    Started development of Tutorial level. Implemented main menu design & most of the functionality.

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,10 +1,110 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public void PlayGame()
+
+    public Slider volumeSlider;
+
+    public Slider brightnessSlider;
+
+    public Image brightnessOverlay;
+    public GameObject mainPanel;
+    public GameObject levelsPanel;
+    public GameObject settingsPanel;
+
+    public GameObject QuitPanel;
+
+    void Start()
+
+    {
+
+        float savedVolume = PlayerPrefs.GetFloat("Volume", 1f);
+
+        float savedBrightness = PlayerPrefs.GetFloat("Brightness", 0.2f);
+
+        volumeSlider.value = savedVolume;
+
+        brightnessSlider.value = savedBrightness;
+
+        SetVolume(savedVolume);
+
+        SetBrightness(savedBrightness);
+
+    }
+
+    public void SetVolume(float value)
+
+    {
+
+        AudioListener.volume = value;
+
+        PlayerPrefs.SetFloat("Volume", value);
+
+    }
+
+    public void SetBrightness(float value)
+
+    {
+
+        Color c = brightnessOverlay.color;
+
+        c.a = Mathf.Lerp(0.45f, 0f, value);
+
+        brightnessOverlay.color = c;
+
+        PlayerPrefs.SetFloat("Brightness", value);
+
+    }
+
+    public void PlayNewGame()
+    {
+        SceneManager.LoadScene("Level1- Tutorial Lobby");
+    }
+
+    public void Continue()
     {
         SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Levels()
+    {
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        QuitPanel.SetActive(false);
+        levelsPanel.SetActive(true);
+        
+    }
+
+    public void Settings()
+    {
+        mainPanel.SetActive(false);
+        levelsPanel.SetActive(false);
+        QuitPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void BackToMain()
+    {
+        levelsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        QuitPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        mainPanel.SetActive(false);
+        levelsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        QuitPanel.SetActive(true);
+        Application.Quit();
     }
 }
--- Assets/UILogics.cs
+++ Assets/UILogics.cs
@@ -11,31 +11,39 @@ public class UIManager : MonoBehaviour
 
     public TMP_Text ammoText;
 
+    public Image brightnessOverlay;
+    public Slider brightnessSlider;
+
     Coroutine hpGlow;
     Coroutine stmGlow;
     Coroutine sanGlow;
 
-    
     private float smoothSpeed = 4f;
     private float targetHealth = 2f;
-    
+
+    void Start()
+    {
+        float savedBrightness = PlayerPrefs.GetFloat("Brightness", 0.5f);
+
+        SetBrightness(savedBrightness);
+
+        if (brightnessSlider != null)
+            brightnessSlider.value = savedBrightness;
+    }
 
     void Update()
-
     {
-
         healthFill.fillAmount =
-
-            Mathf.Lerp(healthFill.fillAmount, targetHealth, Time.deltaTime * smoothSpeed);
-
+            Mathf.Lerp(
+                healthFill.fillAmount,
+                targetHealth,
+                Time.deltaTime * smoothSpeed
+            );
     }
 
     public void UpdateHealth(float current, float max)
-
     {
-
         targetHealth = current / max;
-
     }
 
     public void UpdateStamina(float current, float max)
@@ -52,35 +60,54 @@ public class UIManager : MonoBehaviour
     {
         ammoText.text = current + "/" + max;
 
-        if(current < 1)
-
+        if (current < 1)
             ammoText.color = Color.red;
-
         else
-
             ammoText.color = new Color32(138, 138, 138, 255);
-            }
+    }
 
     public void GlowHealth()
     {
         if (hpGlow != null) StopCoroutine(hpGlow);
-        hpGlow = StartCoroutine(GlowBar(healthFill, new Color(1f, 0.4f, 0.4f)));
+
+        hpGlow = StartCoroutine(
+            GlowBar(healthFill, new Color(1f, 0.4f, 0.4f))
+        );
     }
 
     public void GlowStamina()
     {
         if (stmGlow != null) StopCoroutine(stmGlow);
-        stmGlow = StartCoroutine(GlowBar(staminaFill, new Color(0.4f, 0.7f, 1f)));
+
+        stmGlow = StartCoroutine(
+            GlowBar(staminaFill, new Color(0.4f, 0.7f, 1f))
+        );
     }
 
     public void GlowSanity()
     {
         if (sanGlow != null) StopCoroutine(sanGlow);
-        sanGlow = StartCoroutine(GlowBar(sanityFill, new Color(0.8f, 0.4f, 1f)));
+
+        sanGlow = StartCoroutine(
+            GlowBar(sanityFill, new Color(0.8f, 0.4f, 1f))
+        );
     }
 
+    public void SetBrightness(float value)
+    {
+        if (brightnessOverlay == null) return;
 
-    IEnumerator GlowBar(UnityEngine.UI.Image bar, Color glowColor)
+        Color c = brightnessOverlay.color;
+
+        c.a = Mathf.Lerp(0.45f, 0f, value);
+
+        brightnessOverlay.color = c;
+
+        PlayerPrefs.SetFloat("Brightness", value);
+        PlayerPrefs.Save();
+    }
+
+    IEnumerator GlowBar(Image bar, Color glowColor)
     {
         Color original = bar.color;
 
